cd920f83ff3d431cda034610cd0faffe61eab8aa
andrdc <andrsruiz@gmail.com>
2023-05-06 19:26:19 -0600

    Initial commit

---
+++ chessboard/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ChessBoard from './components/ChessBoard.vue';
+import SideBar from './components/SideBar.vue';
+</script>
+
+<template>
+  <ChessBoard />
+  <SideBar />
+</template>
---
+++ chessboard/src/components/SideBar.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>SideBar</div>
+</template>
